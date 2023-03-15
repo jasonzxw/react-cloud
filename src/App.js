7b23ee1258777af77da58636b99eea2abce66ac2
jason_zuo <jason_zuo@trendmicro.com>
2023-03-15 09:56:12 +0800

    init cloud-react dev project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <aside className='App-aside'>
+        <div className='aside-contianer'>
+          <p>欢迎来到jason的个人空间</p>
+        </div>
+      </aside>
+      <main className='App-main'>
+
+      </main>
+      {/* <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
