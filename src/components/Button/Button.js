8c4d77f123f43bec512bb3b443959ebcafa82118
jason_zuo <jason_zuo@trendmicro.com>
2023-03-20 16:46:35 +0800

    画板增加擦除、粗细、颜色、优化锯齿但不明显

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,11 +1,19 @@
+/*
+ * @author: jason_zuo
+ * @LastEditors: jason_zuo
+ * @LastEditTime: 2023-03-20 14:40:30
+ * @FilePath: \react-cloud\src\components\Button\Button.js
+ */
+import {forwardRef} from 'react'
 import './Button.css'
 
-const Button = ({text,click,type='btn-default'}) => {
+const Button = forwardRef((props,ref = null) => {
+    const {text,click,type='btn-default' , id=""} = props ;
     return (
-        <div className={['btn-primary',type].join(' ')} onClick={click}>
+        <div className={['btn-primary',type].join(' ')} onClick={click} ref={ref} id={id}>
             {text}
         </div>
     )
-}
+})
 
 export default Button
--- src/components/DrawPanel/DrawPanel.js
+++ src/components/DrawPanel/DrawPanel.js
@@ -1,48 +1,74 @@
 /*
  * @author: jason_zuo
  * @LastEditors: jason_zuo
- * @LastEditTime: 2023-03-17 17:04:55
+ * @LastEditTime: 2023-03-20 16:44:36
  * @FilePath: \react-cloud\src\components\DrawPanel\DrawPanel.js
  */
 import { useEffect, useRef } from "react";
 import "./DrawPanel.css";
 import Download from "../DownLoad/Download";
 import { getCurrentDate } from "../../Utils/date";
+import Button from "../Button/Button";
 const DrawPanel = ({ width = 100, height = 300 }) => {
   let colorRef = useRef();
+  let backgroundColorRef = useRef()
   let canvasRef = useRef();
   let isDarwingRef = useRef(false);
+  let isCleaningRef = useRef(false);
   let darwStartPosition = useRef();
+  let drawLinwWidth = useRef(2.0) ;
+  let cleanStarPosition = useRef();
 
   useEffect(() => {
     let drawContainerEl = document.getElementById("draw-container");
     let canvasEl = document.getElementById("draw-panel");
-    canvasEl.width = drawContainerEl.clientWidth;
+    let width = (canvasEl.width = drawContainerEl.clientWidth);
+    let { height } = canvasEl.getBoundingClientRect();
+    // 解决锯齿但是不明显
+    const init = () => {
+      const dlp = window.devicePixelRatio;
+      const screenScale = window.screen.width / 750
+      canvasEl.style.width = width + "px";
+      canvasEl.style.height = height + "px";
+
+      canvasEl.width = parseInt(screenScale * width) * dlp
+      canvasEl.height = parseInt(screenScale * height) * dlp
+
+    //   canvasEl.width = width * dlp;
+    //   canvasEl.height = height * dlp;
+      let ctx = canvasEl.getContext("2d");
+      ctx.scale(devicePixelRatio * screenScale, devicePixelRatio * screenScale);
+    };
+    init();
 
     const drawStatrt = (e) => {
-      isDarwingRef.current = true;
-      const isTouch = e instanceof TouchEvent ;
-      const {left,top} = canvasEl.getBoundingClientRect();
+      if (!isCleaningRef.current) {
+        isDarwingRef.current = true;
+      }
       darwStartPosition.current = {
-        x: isTouch?  e.touches[0].clientX - left: e.offsetX,
-        y: isTouch?  e.touches[0].clientY -top : e.offsetY,
+        x: e.offsetX,
+        y: e.offsetY,
       };
       let canvasCtxRef = canvasRef.current.getContext("2d");
-      canvasCtxRef.strokeStyle  = colorRef.current.value;
+      canvasCtxRef.lineWidth  = drawLinwWidth.current;
+      canvasCtxRef.strokeStyle = colorRef.current.value;
       canvasCtxRef.beginPath();
     };
+
     const remove = (e) => {
       if (isDarwingRef.current) {
-        const isTouch = e instanceof TouchEvent ;
         let ctx = canvasRef.current.getContext("2d");
-        const {left,top} = canvasEl.getBoundingClientRect();
         ctx.moveTo(darwStartPosition.current.x, darwStartPosition.current.y);
-        isTouch? ctx.lineTo(e.touches[0].clientX -left ,e.touches[0].clientY - top ) : ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.lineTo(e.offsetX, e.offsetY);
         ctx.stroke();
         darwStartPosition.current = {
-            x: isTouch?  e.touches[0].clientX - left: e.offsetX,
-            y: isTouch?  e.touches[0].clientY -top: e.offsetY,
+          x: e.offsetX,
+          y: e.offsetY,
         };
+      }
+      if (isCleaningRef.current) {
+        let ctx = canvasRef.current.getContext("2d");
+        ctx.clearRect(e.offsetX, e.offsetY, 10, 10);
       }
     };
 
@@ -51,22 +77,62 @@ const DrawPanel = ({ width = 100, height = 300 }) => {
       isDarwingRef.current = false;
       canvasCtxRef.closePath();
     };
+
+    const mobileDrawStatrt = (e) => {
+      e.preventDefault();
+      if (!isCleaningRef.current) {
+        isDarwingRef.current = true;
+      }
+      const { left, top } = canvasEl.getBoundingClientRect();
+      darwStartPosition.current = {
+        x: e.touches[0].clientX - left,
+        y: e.touches[0].clientY - top,
+      };
+      let canvasCtxRef = canvasRef.current.getContext("2d");
+      canvasCtxRef.lineWidth  = drawLinwWidth.current;
+      canvasCtxRef.strokeStyle = colorRef.current.value;
+      canvasCtxRef.beginPath();
+    };
+
+    const mobileRemove = (e) => {
+      e.preventDefault();
+      if (isDarwingRef.current) {
+        let ctx = canvasRef.current.getContext("2d");
+        const { left, top } = canvasEl.getBoundingClientRect();
+        ctx.moveTo(darwStartPosition.current.x, darwStartPosition.current.y);
+        ctx.lineTo(e.touches[0].clientX - left, e.touches[0].clientY - top);
+        ctx.stroke();
+        darwStartPosition.current = {
+          x: e.touches[0].clientX - left,
+          y: e.touches[0].clientY - top,
+        };
+      }
+      if (isCleaningRef.current) {
+        let ctx = canvasRef.current.getContext("2d");
+        const { left, top } = canvasEl.getBoundingClientRect();
+        ctx.clearRect(
+          e.touches[0].clientX - left,
+          e.touches[0].clientY - top,
+          10,
+          10
+        );
+      }
+    };
+
+    const mobileEndDraw = (e) => {
+      e.preventDefault();
+      let canvasCtxRef = canvasRef.current.getContext("2d");
+      isDarwingRef.current = false;
+      canvasCtxRef.closePath();
+    };
+
     canvasEl.addEventListener("mousedown", drawStatrt);
     canvasEl.addEventListener("mousemove", remove);
     canvasEl.addEventListener("mouseup", endDraw);
     canvasEl.addEventListener("mouseleave", endDraw);
-    canvasEl.addEventListener("touchstart", (e)=> {
-        e.preventDefault()
-        drawStatrt(e)
-    });
-    canvasEl.addEventListener("touchmove", (e) => {
-        e.preventDefault()
-        remove(e)
-    });
-    canvasEl.addEventListener("touchend", (e) =>{
-        e.preventDefault()
-        endDraw(e)
-    });
+    canvasEl.addEventListener("touchstart", mobileDrawStatrt);
+    canvasEl.addEventListener("touchmove", mobileRemove);
+    canvasEl.addEventListener("touchend", mobileEndDraw);
   }, []);
 
   const generateUrl = () => {
@@ -80,15 +146,101 @@ const DrawPanel = ({ width = 100, height = 300 }) => {
     return fileName + "-" + year + "-" + month + "-" + day;
   };
 
-  const changeColor = () => {
-    console.log(colorRef.current.value)
+
+
+  const showColorExample = () => {
+    // const el = document.getElementById("color-example");
+    // el.style.display = "inline-block";
+    // el.style.backgroundColor = colorRef.current.value;
+    const btn = document.getElementById('btn_colorpen')
+    btn.style.backgroundColor = colorRef.current.value;
+    btn.style.borderColor = colorRef.current.value;
+  };
+
+
+
+  const showCanvasBackgroundColor = () => {
+    const el = document.getElementById("draw-panel");
+    el.style.borderColor = backgroundColorRef.current.value;
+    el.style.borderWidth = '1px'
+    el.style.backgroundColor = backgroundColorRef.current.value;
+
+    const btn = document.getElementById('btn_backgrouncolor');
+    btn.style.backgroundColor = backgroundColorRef.current.value;
+    btn.style.borderColor = backgroundColorRef.current.value;
+  };
+
+  const changeColor = (e) => {
+    colorRef.current.click();
+    actionAnimaton();
+    e.target.classList.add('btn_active');
+  };
+
+  const changeBackgroundColor = (e) => {
+    backgroundColorRef.current.click();
+    actionAnimaton();
+    e.target.classList.add('btn_active');
+  };
+
+  const enableCleaneStatus = (e) => {
+    isDarwingRef.current = false;
+    isCleaningRef.current = true;
+    actionAnimaton();
+    e.target.classList.add('btn_active');
+  };
+
+  const disableCleaneStatus = (e) => {
+    isCleaningRef.current = false;
+    actionAnimaton();
+    e.target.classList.add('btn_active');
+  };
+
+  const expandLineWidth = (e) => {
+    actionAnimaton();
+    drawLinwWidth.current += 2.0
+    e.target.classList.add('btn_active');
+
   }
-  
+  const cleanScreen = (e) => {
+    actionAnimaton();
+    let canvasCtxRef = canvasRef.current.getContext("2d");
+    canvasCtxRef.clearRect(0,0,canvasRef.current.width,canvasRef.current.height)
+    e.target.classList.add('btn_active');
+  }
+
+  const actionAnimaton = () => {
+    const btn_ilst = ['btn_colorpen' ,'btn_colorclean','btn_draw' , 'btn_backgrouncolor','btn_linewidth','btn_clearscreen'];
+    btn_ilst.forEach(id => {
+        const el = document.getElementById(id);
+        el.classList.remove('btn_active');
+    })
+  }
+
+
   return (
     <div className="draw-container" id="draw-container">
-      <div className="color-select">
-        <label>Choose color &nbsp;&nbsp;&nbsp;</label>
-        <input type="color" ref={colorRef} onChange={changeColor}/>
+      <div className="draw-action">
+        <div className="color-select">
+          <Button text={"笔颜色"} click={changeColor} id='btn_colorpen'/>
+          <span id="color-example" className="color-example"></span>
+          <input type="color" ref={colorRef} onChange={showColorExample}/>
+        </div>
+        <div className="clean-select">
+          <Button text={"橡皮擦"} click={enableCleaneStatus} id='btn_colorclean'/>
+        </div>
+        <div className="backgroundcolor-select">
+          <Button text={"背景色"} click={changeBackgroundColor} id='btn_backgrouncolor'/>
+          <input type="color" ref={backgroundColorRef} onChange={showCanvasBackgroundColor} />
+        </div>
+        <div className="draw-select">
+          <Button text={"画画"} click={disableCleaneStatus} id='btn_draw'/>
+        </div>
+        <div className="linewidth-select">
+          <Button text={"笔粗细"} click={expandLineWidth} id='btn_linewidth'/>
+        </div>
+        <div className="clearscreen-select">
+          <Button text={"清屏"} click={cleanScreen} id='btn_clearscreen'/>
+        </div>
       </div>
       <canvas
         className="draw-panel"
